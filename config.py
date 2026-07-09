f1a8baaae07e399ae34bcb8c64a1cf0e18f2dc5f
n3ym4r㉿gen <kehindeadigun219@gmail.com>
2026-07-09 15:27:24 +0100

    Use PostgreSQL on Render

--- config.py
+++ config.py
@@ -3,21 +3,18 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
-
 os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 
 class Config:
-
     SECRET_KEY = os.environ.get(
         "SECRET_KEY",
         "attendance-secret-key"
     )
 
-    SQLALCHEMY_DATABASE_URI = \
-        "sqlite:///" + os.path.join(
-            INSTANCE_DIR,
-            "attendance.db"
-        )
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "attendance.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
